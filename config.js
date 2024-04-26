7988db4570e738f6873359812eee664586ebbd59
Mohd221 <168160810+Mohd221@users.noreply.github.com>
2024-04-26 21:40:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = ['2347045035241', 'Excel', true]
+global.owner = ['2348163717090', 'Binali', true]
  //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2347045035241'] 
-global.prems = ['2347045035241']
-global.allowed = ['2347045035241']
+global.mods = ['2348163717090'] 
+global.prems = ['2348163717090']
+global.allowed = ['2348163717090']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -49,16 +49,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'STAR-V2'
+global.botname = '𝐁𝐈𝐍𝐀𝐋𝐈 𝐌𝐃-𝐁𝐎𝐓 𝐯𝟑'
 global.premium = 'true'
-global.packname = 'EX-BOTS_TEAM' 
-global.author = '@EXCEL' 
+global.packname = '𝐈𝐭𝐳 𝐁𝐢𝐧 𝐀𝐥𝐢 ˡᵉᵍᵉⁿᵈ' 
+global.author = '@Binali' 
 global.menuvid = 'https://i.imgur.com/p7sdeTT.mp4'
-global.igfg = '▢✓ Follow My channel_ https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m\n' 
+global.igfg = '▢✓ Follow My channel_ https://whatsapp.com/channel/0029VacbEHvF1YlWF4XgyO13\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
 global.fgsc = 'https://github.com/Xcelsama/STAR-MD-V2' 
-global.fgyt = 'https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m'
-global.fgpyp = 'https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m'
+global.fgyt = 'https://whatsapp.com/channel/0029VacbEHvF1YlWF4XgyO13'
+global.fgpyp = 'https://whatsapp.com/channel/0029VacbEHvF1YlWF4XgyO13'
 global.fglog = 'STAR.jpg' 
 global.thumb = fs.readFileSync('./STAR.jpg')
 
